7b6f572c8c484a247ba64e5f018c1d55b771293d
ngovea <ngovea@umich.edu>
2025-07-29 15:31:56 -0700

    changing to streamlit

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
 import pandas as pd
-
-#Read CSV data in Python
-df = pd.read_csv('data.csv')
-
+import numpy as np
 import matplotlib.pyplot as plt
+import streamlit  as st
 
-# Convert 'Date' to datetime, if it's not already
-df['Date'] = pd.to_datetime(df['Date'])
-
-# Plot 'Hour Slept' (handle possible missing/blank data)
-df['Hour Slept'] = pd.to_numeric(df['Hour Slept'], errors='coerce')
-
-plt.figure(figsize=(10, 6))
-plt.plot(df['Date'], df['Hour Slept'])
-plt.xlabel('Date')
-plt.ylabel('Hours Slept')
-plt.title('Sleep Over Time')
-plt.tight_layout()
-plt.show()
+st.write("hello world")
