ace56d191a23c6396dd3923cbaa72382fe111ee8
ngovea <ngovea@umich.edu>
2025-07-29 15:46:26 -0700

    MVP Chart

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
 import streamlit  as st
+import math
+import matplotlib.pyplot as plt
 
-st.write("hello world")
+data = pd.read_csv("data.csv")
+data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%y', errors='coerce')
+data = data.set_index('Date')
+#Title
+st.title("Personal Data Tracker")
+#Shows the Data
+st.line_chart(data)
+st.write(data)
+
